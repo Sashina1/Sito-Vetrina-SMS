eada8b7609da5d00b98503befb75a5e120ac6ae8
Sasha <sasha9107@libero.it>
2024-10-08 11:13:50 +0200

    installato bootstrap, creato componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'app-home',component: HomeComponent},
+    {path: '', redirectTo: '/app-home', pathMatch: 'full'}
+];
